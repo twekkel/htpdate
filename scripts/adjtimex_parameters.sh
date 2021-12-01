4389c0535078e529191490b7e8487f346133706d
Eddy Vervest <eddy@vervest.org>
2021-12-01 17:30:34 +0100

    Update adjtimex script

--- scripts/adjtimex_parameters.sh
+++ scripts/adjtimex_parameters.sh
@@ -2,17 +2,15 @@
 #
 # In daemon mode htpdate writes the systematic drift of the clock to syslog.
 # With this script you can convert the PPM drift values into adjtimex
-# parameters. Use it when you know what you are doing...
+# parameters. Only use this script when you know what you are doing.
 #
-# The Linux adjtimex manpage gives you more information.
-#
-# Feel free to contribute for other OS's.
+# Consult adjtimex man page for more information.
 
 if [ ! $1 ]; then
-	echo "Usage: $0 <PPM> [current TICK] [current FREQ]"
-	echo "By default TICK=10000 and FREQ=0"
-	echo
-	exit 1
+    echo "Usage: $0 <PPM> [current TICK] [current FREQ]"
+    echo "By default TICK=10000 and FREQ=0"
+    echo
+    exit 1
 fi
 
 PPM=$1
@@ -20,11 +18,11 @@ TICK=10000
 FREQ=0
 
 if [ $2 ]; then
-	TICK=$2
+    TICK=$2
 fi
-	
+    
 if [ $3 ]; then
-	FREQ=$3
+    FREQ=$3
 fi
 
 FREQTOT=`echo "$PPM * 65536 + $TICK * 6553600 + $FREQ" | bc`
